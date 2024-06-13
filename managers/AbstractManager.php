1f6f1adf25be3c355b79864932b2bccbf436450f
Marion1b <121030423+Marion1b@users.noreply.github.com>
2024-06-13 17:12:25 +0200

    feat: style about page (#8)

--- managers/AbstractManager.php
+++ managers/AbstractManager.php
@@ -1,16 +1,32 @@
 <?php
 
-abstract class AbstractManager
-{
-
+abstract class AbstractManager {
+    
     protected PDO $db;
-
     
     public function __construct(){
         
         $host = "localhost";
     $port = "3306";
-    $dbname = "benjaminhaverbeke_distorision";
+    $dbname = "distorsion";
     $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
+    $user = "root";
+    $password = "";
+        
+        
+        $this->db = new PDO(
+    $connexionString,
+    $user,
+    $password);
+        
+        
+    }
+    
+    
+    
 }
+
+
+
+?>
