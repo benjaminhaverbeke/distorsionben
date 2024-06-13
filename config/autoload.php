e9f699f0c312b31b88465050db9ae1a2d032a972
benjaminhaverbeke <159901606+benjaminhaverbeke@users.noreply.github.com>
2024-06-13 14:02:33 +0200

    autoloadaddrequire (#3)

--- config/autoload.php
+++ config/autoload.php
@@ -1,5 +1,15 @@
 <?php
 
+
+/**Models */
+
+
+require "models/Category.php";
+require "models/Message.php";
+require "models/Salon.php";
+require "models/User.php";
+
+
 /**Controllers */
 require "controllers/PageController.php";
 require "controllers/ChatController.php";
@@ -7,5 +17,23 @@ require "controllers/AuthController.php";
 
 /**Router */
 require "config/Router.php";
+
+/**Managers */
+
+require "managers/AbstractManager.php";
+require "managers/CategoryManager.php";
+require "managers/MessageManager.php";
+require "managers/SalonManager.php";
+require "managers/UserManager.php;";
+
+/**Controller */
+
+require "controllers/AdminController.php";
+require "controllers/AuthController.php";
+require "controllers/ChatController.php";
+require "controllers/PageController.php";
+
+
+/** 
 
 ?>
