942ed345f30489fb13d94ccd75fb0ddcfc767e23
benjamin.haverbeke <benjamin.haverbeke@outlook.com>
2024-07-01 10:06:24 +0000

    base distorsion connexion ok

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 
 session_start();
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 require "config/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+
+
 
 $router=new Router();
 $router->handleRequest($_GET);
--- managers/AbstractManager.php
+++ managers/AbstractManager.php
@@ -6,10 +6,20 @@ abstract class AbstractManager
 
     public function __construct()
     {
-        $this->db = new PDO(
-            "mysql:host=localhost;port=3306;dbname=distorsion",
-            "root",
-            ""
-        );
+        
+        $dbHost = $_ENV['DB_HOST'];
+        $dbUser = $_ENV['DB_USER'];
+        $dbPass = $_ENV['DB_PASS'];
+        $dbName = $_ENV['DB_NAME'];
+        
+        try {
+    $this->db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    // Définir le mode d'erreur PDO à exception
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie";
+    } catch(PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+        
     }
 }
